14d4083ad3c03f197151479d8aa1e3961e00351f
Oualid Amch <oualid.amchayd@sofrecom.com>
2020-07-17 11:51:41 +0100

    remove product

--- src/main/java/com/springboottuto/camunda/controllers/ProductController.java
+++ src/main/java/com/springboottuto/camunda/controllers/ProductController.java
@@ -18,7 +18,6 @@ public class ProductController {
 		products.add("Sumsung");
 		products.add("LG");
 		products.add("Lenevo");
-		products.add("Iphone");
 	}
 	@GetMapping("/product/{prd}")
 	public String displayProduct( @PathVariable("prd") String prd) {
